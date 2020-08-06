2775b99ae88bd0041cae92e0b488ca0744f76065
gaochongyang <gaochongyang@enmonster.com>
2020-08-06 19:14:15 +0800

    test

---
+++ CYTestLib/Classes/CYBaseView/CYBaseView.h
@@ -0,0 +1,16 @@
+//
+//  CYBaseView.h
+//  CYTestLib
+//
+//  Created by enmonster on 2020/8/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CYBaseView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CYTestLib/Classes/CYBaseView/CYBaseView.m
@@ -0,0 +1,20 @@
+//
+//  CYBaseView.m
+//  CYTestLib
+//
+//  Created by enmonster on 2020/8/6.
+//
+
+#import "CYBaseView.h"
+
+@implementation CYBaseView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
